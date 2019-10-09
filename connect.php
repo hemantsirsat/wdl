8dfaf6945b53eda4b923ea3d46a38bad57aa775e
hemantsirsat <36109613+hemantsirsat@users.noreply.github.com>
2019-10-09 22:12:45 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,33 +1,28 @@
 <?php
-	$name = filter_input(INPUT_POST, 'name');
-	$email = filter_input(INPUT_POST, 'email');
-	$phone = filter_input(INPUT_POST, 'phone');
-	$interest = filter_input(INPUT_POST, 'interest');
-	$expectation = filter_input(INPUT_POST, 'expectations');
-
-	if (!empty($username)){
-	if (!empty($password)){
-	$host = "127.0.0.1";
-	$dbusername = "root";
-	$dbpassword = "";
-	$dbname = "tourism";
-	// Create connection
-	$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-	if (mysqli_connect_error()){
-	die('Connect Error ('. mysqli_connect_errno() .') '
-	. mysqli_connect_error());
-	}
-	else{
-	$sql = "INSERT INTO customer_info (full_name, email,phone,interest,expectations)
-	values ('$name','hemant@ieee.org','$phone','$interest','$expectation')";
-	if ($conn->query($sql)){
-	echo "New record is inserted sucessfully";
-	}
-	else{
-	echo "Error: ". $sql ."
-	". $conn->error;
-	}
-	$conn->close();
-	}
-	}
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("127.0.0.1", "root", "", "tourism");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$first_name = (isset($_POST['name']) ? $_POST['name'] : '');
+$email = (isset($_POST['email']) ? $_POST['email'] : '');
+$phone = (isset($_POST['phone']) ? $_POST['phone'] : '');
+$interest = (isset($_POST['interest']) ? $_POST['interest'] : '');
+$expectations = (isset($_POST['expectations']) ? $_POST['expectations'] : '');
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO customer_info (full_name,email,phone,interest,expectations) VALUES ('$first_name', '$email', '$phone', '$interest', '$expectations')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
 ?>
