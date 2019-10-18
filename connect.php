81d19fc1915823333d63ee22c677950a992a772a
hemantsirsat <36109613+hemantsirsat@users.noreply.github.com>
2019-10-18 15:12:36 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("127.0.0.1", "root", "", "tourism");
+$link = mysqli_connect("localhost", "root", "", "tourism");
  
 // Check connection
 if($link === false){
